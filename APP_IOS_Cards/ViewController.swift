7e9abbbaeb4d3a5801bd9d5fdfe3bd13e6ad48ff
Kateryna Kozlova <kozlovakaterina1@gmail.com>
2019-05-11 10:40:37 +0200

    add new project

---
+++ APP_IOS_Cards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APP_IOS_Cards
+//
+//  Created by Kateryna Kozlova on 11/04/2019.
+//  Copyright © 2019 Kateryna Kozlova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
